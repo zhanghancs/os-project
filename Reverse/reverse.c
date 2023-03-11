7f8725f037d81fc9864d851ce97763545c3dd824
zhanghan <zhanghan.coder@outlook.com>
2023-03-11 22:01:32 +0800

    solve reverse using getline, leave malloc fault

--- Reverse/reverse.c
+++ Reverse/reverse.c
@@ -25,62 +25,88 @@ main(int argc, char* argv[]) {
 		fprintf(stderr, "usage: reverse <input> <output>\n");
 		exit(1);
 	}
+	// 链表的头，从文件中读取的信息
 	ListNode* head = NULL;
-	char buf[255];
+	char* buf = NULL;
+	size_t length = 0;
+	size_t len = 0;
+	// 第一种情况
 	if (argc == 1) {
-		while (fgets(buf, 255, stdin) != NULL) {
+		while ((len = getline(&buf, &length, stdin)) != -1) {
 			ListNode* temp = (ListNode*)malloc(sizeof(ListNode));
 			temp->next = head;
 			temp->buf = (char*)malloc(strlen(buf) + 1);
 			strcpy(temp->buf,buf);
 			head = temp;
+			// free(buf);
 		}
 		// head = reverse(head);
 		while (head) {
-				fprintf(stdout, "%s", head->buf);
-				head = head->next;
-			}
-	} else {
+			fprintf(stdout, "%s", head->buf);
+			ListNode* temp = head;
+			head = head->next;
+			free(temp);
+		}
+	} else if (argc == 2) {
 		FILE* input = fopen(argv[1], "r");
 		if (input == NULL) {
 			fprintf(stderr, "reverse: cannot open file '%s'\n",argv[1]);
 			exit(1);
 		}
-		while (fgets(buf, 255, input) != NULL) {
+		while ((len = getline(&buf, &length, input)) != -1) {
 			ListNode* temp = (ListNode*)malloc(sizeof(ListNode));
 			temp->next = head;
 			temp->buf = (char*)malloc(strlen(buf) + 1);
 			strcpy(temp->buf,buf);
 			head = temp;
+			// free(buf);
 		}
 		// head = reverse(head);
 		fclose(input);
-		if (argc == 2) {
-			while (head) {
-				fprintf(stdout, "%s", head->buf);
-				head = head->next;
-			}
-		}
-		else if (argc == 3) {
-			FILE* output = fopen(argv[2], "w");
-			if (output == NULL) {
-				fprintf(stderr, "reverse: cannot open file '%s'\n",argv[2]);
-				exit(1);
-			}
-			// 获取文件信息
-			struct stat in, out;
-			stat(argv[1], &in);
-			stat(argv[2], &out);
-			if (in.st_ino == out.st_ino && in.st_dev == out.st_dev) {
-				fprintf(stderr, "reverse: input and output file must differ\n");
-				exit(1);
-			}
-			while (head) {
-				fprintf(output, "%s", head->buf);
-				head = head->next;
-			}
-			fclose(output);
+		while (head) {
+			fprintf(stdout, "%s", head->buf);
+			ListNode* temp = head;
+			head = head->next;
+			free(temp);
 		}
 	}
+	else if (argc == 3) {
+		FILE* input = fopen(argv[1], "r");
+		if (input == NULL) {
+			fprintf(stderr, "reverse: cannot open file '%s'\n",argv[1]);
+			exit(1);
+		}
+		FILE* output = fopen(argv[2], "w");
+		if (output == NULL) {
+			fprintf(stderr, "reverse: cannot open file '%s'\n",argv[2]);
+			exit(1);
+		}
+		// 获取文件信息
+		struct stat in, out;
+		stat(argv[1], &in);
+		stat(argv[2], &out);
+		if (in.st_ino == out.st_ino && in.st_dev == out.st_dev) {
+			fprintf(stderr, "reverse: input and output file must differ\n");
+			exit(1);
+		}
+
+		while ((len = getline(&buf, &length, input)) != -1) {
+			ListNode* temp = (ListNode*)malloc(sizeof(ListNode));
+			temp->next = head;
+			temp->buf = (char*)malloc(strlen(buf) + 1);
+			strcpy(temp->buf,buf);
+			head = temp;
+			// free(buf);
+		}
+		fclose(input);
+		while (head) {
+			fprintf(output, "%s", head->buf);
+			ListNode* temp = head;
+			head = head->next;
+			free(temp);
+		}
+		fclose(output);
+	}
+	free(buf);
 	exit(0);
 }
